4c1caa095808be010b608c3d14babafba4b72493
srinivas <tvskumar2000@hotmail.com>
2017-10-24 20:22:42 +0530

    Updated git Ignore

---
+++ LibraryDbProject/BookInfo.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[BookInfo]
+(
+	[BookId]		INT PRIMARY KEY IDENTITY(1,1), 
+    [BookName]		VARCHAR(200), 
+    [BookPrice]		INT, 
+    [BookAuthor]	VARCHAR(50), 
+    [BookReview]	INT
+)
